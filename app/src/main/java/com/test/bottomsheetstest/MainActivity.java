f20ca810a0df9bd92e70c0de64250074b8220f7e
Yaroslav Yadrov <yaroslav.yadrov@gmail.com>
2017-02-03 21:50:55 +0300

    Доделал

--- app/src/main/java/com/test/bottomsheetstest/MainActivity.java
+++ app/src/main/java/com/test/bottomsheetstest/MainActivity.java
@@ -2,8 +2,11 @@ package com.test.bottomsheetstest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -12,6 +15,11 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.showDialogButton) Button showDialogButton;
+    @BindView(R.id.titleEditText) EditText titleEditText;
+    @BindView(R.id.positiveEditText) EditText positiveEditText;
+    @BindView(R.id.negativeEditText) EditText negativeEditText;
+    @BindView(R.id.cancellableCheckBox) AppCompatCheckBox cancellableCheckBox;
+    @BindView(R.id.imageCheckBox) AppCompatCheckBox imageCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,30 +29,37 @@ public class MainActivity extends AppCompatActivity {
         showDialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDialog();
+                if (!TextUtils.isEmpty(positiveEditText.getText()) &&
+                        !TextUtils.isEmpty(titleEditText.getText())) {
+                    showDialog();
+                } else {
+                    Toast.makeText(MainActivity.this, "error: enter text", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
     private void showDialog() {
         new BottomDialog.Builder(this, getSupportFragmentManager())
-                .cancellable(true)
-                .title("sdfsdfsdfs")
-                .positiveText("YES")
-                .negativeText("NO")
+                .cancellable(cancellableCheckBox.isChecked())
+                .title(titleEditText.getText().toString())
+                .positiveText(positiveEditText.getText().toString())
+                .negativeText(negativeEditText.getText().toString())
                 .positiveClick(new BottomDialog.ButtonClickListener() {
                     @Override
                     public void click(BottomDialog dialog) {
-                        Toast.makeText(MainActivity.this, "positive", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "positive button clicked", Toast.LENGTH_SHORT).show();
                         dialog.dismiss();
                     }
                 })
                 .negativeClick(new BottomDialog.ButtonClickListener() {
                     @Override
                     public void click(BottomDialog dialog) {
-                        Toast.makeText(MainActivity.this, "negative", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "negative button clicked", Toast.LENGTH_SHORT).show();
                         dialog.dismiss();
                     }
                 })
+                .image(imageCheckBox.isChecked() ? R.drawable.image : 0)
                 .show();
     }
 }
